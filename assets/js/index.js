3424e8f8f57bc002715b4891194321563f51237d
David Miranda <dvdfermiranda@yahoo.com.mx>
2024-05-06 19:32:16 -0600

    Changed top cryptos from 10 to 5

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,7 @@ function getGeckoApi() {
         method: 'GET',
         headers: { accept: 'application/json', 'x-cg-demo-api-key': 'CG-JRFGKiLZjLqxhCoT7of1Kng2' }
     };
-    const requestUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1';
+    const requestUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=1';
 
     fetch(requestUrl, options)
         .then(function (response) {
@@ -52,7 +52,7 @@ function fetchCurrencyInfo() {
             // Display currency information
             const currencies = data.rates;
             const rightColumn = document.querySelector('.right-column');
-            console.log(data);  
+            console.log(data);
 
             for (const currency in currencies) {
                 const card = document.createElement('div');
