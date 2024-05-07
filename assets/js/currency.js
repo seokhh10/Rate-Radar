e059a9c1fef5a2834c8febf912b94832df121e6e
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-05-07 14:20:28 -0500

    I const a requestUrl to get data. I added a fetch, used console to display data. still not working

--- assets/js/currency.js
+++ assets/js/currency.js
@@ -1,5 +1,29 @@
-fetch("https://api.currencyfreaks.com/v2.0/supported-currencies")
-.then (response => response.json())
-.then (data => console.log(data))
-.catch(error => console.error(error));
+const textInput = document.getElementById('text-input');
+const fetchButton = document.getElementById('fetch-button');
+const results = document.getElementById('results')
 
+function fetchCurrencyInfo(event) {
+    const options = {
+        method: 'GET',
+        headers: { accept: 'application/json'}
+    };
+    const currencyName = textInput.value;
+    const requestUrl = `https://api.currencyfreaks.com/v2.0/rates/latest?apikey=d463c482a136446b91438de5e6d4f46c&symbols=${currencyName}`;
+
+    fetch(requestUrl, options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            results.innerHTML = '';
+
+        }
+
+function fetchButtonHandler(event) {
+    event.preventDefault();
+    fetchCurrencyInfo();
+}
+
+fetchButton.addEventListener('click', fetchButtonHandler);
